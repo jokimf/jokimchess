9e95fa69ccceb041147b047d1e30cafc43fb49aa
jokimf <jokim@fode.de>
2022-10-12 18:02:29 +0200

    alphabeta pruning

--- src/main/java/im/jok/jokimchess/evaluation/Evaluator.java
+++ src/main/java/im/jok/jokimchess/evaluation/Evaluator.java
@@ -9,7 +9,7 @@ import im.jok.jokimchess.chessboard.PieceColor;
 public class Evaluator {
 
     public Move determineBestMove(Board b) {
-        EvaluationResult e = minimax(b, 3, b.isTurnWhite());
+        EvaluationResult e = alphabeta(b, 3, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, b.isTurnWhite());
         return e.move();
     }
 
@@ -20,7 +20,6 @@ public class Evaluator {
         // Consider all pieces on the board
         for (Piece p : b.getPiecesOnTheBoard()) {
 
-            // Get piece info
             int x = p.getX(), y = p.getY();
 
             double value = 0;
@@ -29,12 +28,12 @@ public class Evaluator {
                 case KNIGHT -> {
                     value = 3;
                     //Knight is in the middle squares
-                    if (x > 2 && x < 6 && y > 2 && y < 6) {
+                    if (x >= 3 && x <= 5 && y >= 3 && y <= 5) {
                         value += 0.5;
                     }
                 }
                 case KING -> {
-                    value = 100;
+                    value = 10;
                     if (fullmoves < 30) {
                         if (x < 1 || x > 6) {
                             value += 1;
@@ -70,8 +69,7 @@ public class Evaluator {
         return sum;
     }
 
-    //TODO: Implement AlphaBeta Pruning
-    public EvaluationResult minimax(Board b, int depth, boolean isMax) {
+    public EvaluationResult alphabeta(Board b, int depth, double alpha, double beta, boolean isMax) {
         GameState g = b.determineGameState();
         switch (g) {
             case ONGOING -> {
@@ -84,22 +82,30 @@ public class Evaluator {
                     EvaluationResult max = null;
                     for (Move m : b.allPossibleMoves(true)) {
                         b.playMove(m);
-                        EvaluationResult eval = minimax(b, depth - 1, false);
+                        EvaluationResult eval = alphabeta(b, depth - 1, alpha, beta, false);
                         b.undoLastMove();
                         if (max == null || eval.eval().compareTo(max.eval()) > 0) {
                             max = new EvaluationResult(eval.eval(), m);
                         }
+                        if (max.eval().getEval() >= beta) { // TODO: Use compareTo
+                            break;
+                        }
+                        alpha = Math.max(alpha, max.eval().getEval());
                     }
                     return new EvaluationResult(max.eval().getOneMoveLater(), max.move());
                 } else {
                     EvaluationResult min = null;
                     for (Move m : b.allPossibleMoves(true)) {
                         b.playMove(m);
-                        EvaluationResult eval = minimax(b, depth - 1, true);
+                        EvaluationResult eval = alphabeta(b, depth - 1, alpha, beta, true);
                         b.undoLastMove();
                         if (min == null || eval.eval().compareTo(min.eval()) < 0) {
                             min = new EvaluationResult(eval.eval(), m);
                         }
+                        if (min.eval().getEval() <= alpha) { // TODO: Use compareTo
+                            break;
+                        }
+                        beta = Math.max(beta, min.eval().getEval());
                     }
                     return new EvaluationResult(min.eval().getOneMoveLater(), min.move());
                 }
